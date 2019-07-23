2dd79cf6ed482c5f3c91356f8e20b9ca43b74f75
Cellie <CelliesProjects@users.noreply.github.com>
2019-07-23 20:49:05 +0200

    Update the example.

--- example/FFatSensor/FFatSensor.ino
+++ example/FFatSensor/FFatSensor.ino
@@ -1,24 +1,29 @@
 #include <OneWire.h>
 #include <Task.h>
 #include <FFat.h>
-#include <FFatSensor.h>                                        // 0. Include the library.
+#include <FFatSensor.h>                                        // 0. Include the libraries.
+
+#define FORMAT_FS true /*WILL FORMAT FFAT! set to false to just mount.*/
+
 /* Should be compiled with a FFat partition in Tools>Partition scheme */
 
 FFatSensor sensor;                                             // 1. Make an instance.
 
 void setup() {
   Serial.begin( 115200 );
+  Serial.println();
+  Serial.println();
 
   Serial.println("FFatSensor example sketch. Starting sensors.");
 
   // Logging to FFat
 
-  if ( !FFat.begin() ) Serial.println( "Could not mount FFat.");
+  if ( !FFat.begin( FORMAT_FS ) ) Serial.println( "Could not mount FFat.");
 
   sensor.startSensors( 10, 5 );                                      // 3. Start max 10 DS18B20 sensors on GPIO 5.
 
-  Serial.println("Waiting for first sensor");  // 4. You can just wait until a particular sensor gives a valid reading.
-  while ( sensor.sensorError( 0 ) ) {
+  Serial.print("Waiting for sensors");  // 4. You can just wait until a particular sensor gives a valid reading.
+  while ( !sensor.sensorCount() ) {
     Serial.print(".");
     delay( 100 );
   }
@@ -26,20 +31,27 @@ void setup() {
 
   Serial.printf( "%i sensors found.\n", sensor.sensorCount());   // 5. Or check how many sensors are found.
 
-  Serial.print( "First sensor value: ");                         // 6. Get a sensor reading.
-  Serial.println( sensor.sensorTemp( 0 ) );
+  //Serial.print( "First sensor value: ");                         // 6. Get a sensor reading.
+  //Serial.println( sensor.sensorTemp( 0 ) );
 
 
   // how to get a name and ID
-  sensorId_t   id;                                               // Make a id variable.
-  sensorName_t name;                                             // Make a name variable.
-  Serial.printf( "First sensor id: %s\n", sensor.getSensorId( 0, id ) );       // Get the id and print it in one go.
-  Serial.printf( "First sensor name: %s\n", sensor.getSensorName( 0, name ) ); // Get the name and print it in one go.
+  sensorId_t   id;                   // Make a variable to hold the id.
+  sensorName_t name;                 // Make a variable to hold the name.
+
+  for ( uint8_t num = 0; num < sensor.sensorCount(); num++ ) {
+    // Get the id and print it in one go.
+    Serial.printf( "sensor %i id: %s\n", num, sensor.getSensorId( num, id ) );
+
+    // Get the name and print it in one go.
+    Serial.printf( "sensor %i name: %s\n", num, sensor.getSensorName( num, name ) );
+  }
+
   Serial.println();
 
   //setting a sensor name
   if ( !sensor.setSensorName( id, "thisnameistoolong" ) )        // Rename a sensor. The new name will be stored in NVS and be available after a reboot.
-    sensor.setSensorName( id, "Name is fine!" );                 // Will return true or false depending on the result of the operation.
+    sensor.setSensorName( id, "FFatSensor" );                    // Will return true or false depending on the result of the operation.
 
   Serial.printf( "name from ID: %s is ", id);
   Serial.println( sensor.getSensorName( id, name ) );
@@ -52,7 +64,7 @@ void setup() {
 
     if ( sensor.isTempLogging() ) Serial.println( "Logging already on." );      // You can check the current log state.
 
-    if ( !sensor.startTempLogging( 120 ) ) Serial.println( "Logging already on. (again)" );  // If FFat is mounted sensor values will be logged.
+    if ( !sensor.startTempLogging() ) Serial.println( "Logging already on. (again)" );  // If FFat is mounted sensor values will be logged every 180 seconds.
   }
   else
     Serial.println( "FFat not mounted so no logging enabled." );
